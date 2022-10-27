ae87185ff014305397b2e4098de39865d3c1ed75
tiaggofg <tiago.fernandes.godoy@gmail.com>
2022-10-27 19:47:20 -0300

    First commit

---
+++ src/main/java/com/soft/dp/api/SoftDpApplication.java
@@ -0,0 +1,13 @@
+package com.soft.dp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftDpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftDpApplication.class, args);
+	}
+
+}
